efdf7b3efed7b4b446054443885c07d9266d8de4
hunbalSiddiqui <hunbal.dha@gmail.com>
2020-09-29 16:40:11 +0500

    API CALL DEMO

--- src/AdminAPICalls/AdminAPICalls.js
+++ src/AdminAPICalls/AdminAPICalls.js
@@ -4,4 +4,22 @@ export const fetchResult = (reqObj) =>{
     console.log(reqObj)
 
     //TODO: API CALL
+
+    // return fetch(`${API}/category/create/${userId}`,
+    // header info
+    // {
+    //     method : "POST",
+    //     headers :{
+    //     Accept:"Application/json",
+    //     "Content-Type":"application/json",
+    //     Authorization : `Bearer ${token}`//bearer toekn
+    //     },
+    //     body: JSON.stringify(category)
+    // })
+    // .then(res=>{
+    //     return res.json()
+    // })
+    // .catch(err=>{
+    //     return err
+    // })
 }
